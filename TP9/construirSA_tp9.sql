3f6a6ed337daff42cfcd0ca8c2306f696080aa86
Brayan Arce <arcebrayan@gmail.com>
2025-09-18 19:59:27 -0300

    Add files via upload

---
+++ TP9/construirSA_tp9.sql
@@ -0,0 +1,140 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-09-2025 a las 00:57:06
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `construirsa_tp9`
+--
+CREATE DATABASE IF NOT EXISTS `construirsa_tp9` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `construirsa_tp9`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `id_empleado` int(11) NOT NULL,
+  `dni` bigint(20) NOT NULL,
+  `apellido` varchar(58) NOT NULL,
+  `nombre` varchar(58) NOT NULL,
+  `acceso` int(11) NOT NULL,
+  `estado` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `empleado`
+--
+
+INSERT INTO `empleado` (`id_empleado`, `dni`, `apellido`, `nombre`, `acceso`, `estado`) VALUES
+(1, 42278291, 'Arce', 'Brayan', 3, 1),
+(2, 27384937, 'Gomez', 'Santiago', 2, 1),
+(3, 427483746, 'Aguero', 'Jonatan', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `herramientas`
+--
+
+CREATE TABLE `herramientas` (
+  `id_herramienta` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `descripción` varchar(100) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `estado` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `herramientas`
+--
+
+INSERT INTO `herramientas` (`id_herramienta`, `nombre`, `descripción`, `stock`, `estado`) VALUES
+(1, 'Fresadora', 'máquina-herramienta diseñada para realizar trabajos de mecanizado por arranque de viruta', 5, 1),
+(2, 'Amoladora', 'herramienta eléctrica portátil de alta potencia utilizada principalmente para cortar, lijar, pulir, ', 10, 2),
+(3, 'Taladro', 'herramienta mecánica o eléctrica diseñada para perforar superficies mediante la rotación de una broc', 8, 3),
+(4, 'Soldadora', 'herramienta utilizada principalmente para unir piezas metálicas mediante la aplicación de calor, log', 6, 4),
+(5, 'Martillo', 'herramienta de percusión utilizada para golpear directa o indirectamente una pieza, causando su desp', 11, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `movimiento`
+--
+
+CREATE TABLE `movimiento` (
+  `id_movimiento` int(11) NOT NULL,
+  `id_empleado` int(11) NOT NULL,
+  `id_herramienta` int(11) NOT NULL,
+  `fechap` date NOT NULL,
+  `fechad` date NOT NULL,
+  `cantidadret` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `movimiento`
+--
+
+INSERT INTO `movimiento` (`id_movimiento`, `id_empleado`, `id_herramienta`, `fechap`, `fechad`, `cantidadret`) VALUES
+(1, 1, 3, '2025-04-12', '0000-00-00', 1),
+(2, 1, 5, '2025-04-12', '0000-00-00', 3),
+(3, 2, 1, '2025-01-25', '0000-00-00', 1),
+(4, 2, 2, '2025-01-27', '0000-00-00', 2),
+(5, 3, 4, '2025-10-15', '0000-00-00', 2),
+(6, 3, 5, '2025-11-10', '0000-00-00', 3);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`id_empleado`,`dni`);
+
+--
+-- Indices de la tabla `herramientas`
+--
+ALTER TABLE `herramientas`
+  ADD PRIMARY KEY (`id_herramienta`);
+
+--
+-- Indices de la tabla `movimiento`
+--
+ALTER TABLE `movimiento`
+  ADD PRIMARY KEY (`id_movimiento`),
+  ADD KEY `id_empleado` (`id_empleado`),
+  ADD KEY `id_herramienta` (`id_herramienta`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `movimiento`
+--
+ALTER TABLE `movimiento`
+  ADD CONSTRAINT `movimiento_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`),
+  ADD CONSTRAINT `movimiento_ibfk_2` FOREIGN KEY (`id_herramienta`) REFERENCES `herramientas` (`id_herramienta`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
